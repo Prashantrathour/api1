6fd4d18bca57082354699b56182880d29ab81a11
prashant_rathour <prathour884@gmail.com>
2023-03-10 13:54:06 +0530

    reset

--- serverConfig.js
+++ serverConfig.js
@@ -4,7 +4,7 @@ export default {
     { route: "/posts", methods: ["POST", "PUT", "DELETE", "PATCH"] },
     { route: "/comments", methods: ["POST", "PUT", "DELETE", "PATCH"] },
     { route: "/photos", methods: ["POST", "PUT", "DELETE", "PATCH"] },
-    // { route: "/todos", methods: ["GET", "POST", "PUT", "DELETE", "PATCH"] },
+    { route: "/todos", methods: ["GET", "POST", "PUT", "DELETE", "PATCH"] },
     { route: "/recipeCategories", methods: ["POST", "PUT", "DELETE", "PATCH"] },
     { route: "/recipeIngredients", methods: ["POST", "PUT", "DELETE", "PATCH"] },
     { route: "/recipes", methods: ["POST", "PUT", "DELETE", "PATCH"] },
